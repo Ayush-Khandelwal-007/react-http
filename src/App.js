7cf22c180f48cb882d965b44c81e04d2e54124be
Ayush <ayushkhandelwal3217@gmail.com>
2020-11-17 11:38:06 +0530

    Used react hooks and passed on the fetched data from one component to other sub component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
 import PostForm from './components/postForm';
-import PostList from './components/PostList';
+import PostListBlock from './components/PostListBlock';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { PostsProvider } from './components/postListContext';
 
 function App() {
+
+  const [postlist, setPostlist] = useState();
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/posts`)
+    .then(response => {
+      console.log(response.data);
+      setPostlist(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }, []);
+
   return (
     <div className="App">
       <h1>LIST OF POSTS</h1>
       <header className="App-header">
         <PostForm/>
-        <PostList/>
+        <PostsProvider value={postlist} >
+           <PostListBlock/>
+        </PostsProvider>
       </header>
     </div>
   );
